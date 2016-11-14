3d204488e6684e9f9777966558cd46ea739c9f2a
azwickey-pivotal <vania12R>
2016-11-14 09:58:51 -0500

    building onboard script

--- ci/tasks/onboard-clients.sh
+++ ci/tasks/onboard-clients.sh
@@ -1,26 +1,30 @@
 #!/bin/bash -e
+
+#wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/2804/product_files/8892/download
+#sudo dpkg -i pivotal-gemfire.deb
+
 ls -lha
-wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/2804/product_files/8892/download
-sudo dpkg -i pivotal-gemfire.deb
 
 # Loop through Client teams
-for team in ./*;
-  do
-     [ -d $team ] && cd $team
-     if $team == 'ci'; then
-       continue
-     fi
+find . -maxdepth 1 -type d \( ! -name . \) | while read d; do
+  if [[ $d == *"ci"* || $d == *".git"* ]]; then
+    continue
+  fi
 
-     echo 'Entering into $team'
-     # Add Security
+  echo Entering into $d
+  cd $d
+  # Add Security
 
-     # Add Jars
-     for jar in ./jars/*.jar
-       do
-         #Execute GFSH Command to load jars
-         echo "Loading Jar [$jar] into Gemfire"
-       done;
-
-     # Configure Regions
-
+  # Add Jars
+  for jar in ./jars/*.jar; do
+    #Execute GFSH Command to load jars
+    echo "Loading Jar [$jar] into Gemfire"
+    gfsh \
+    -e "connect --locator=${LOCATOR_CONNECTION}" \
+    -e "deploy --jar=$jar"
   done;
+
+  # Configure Regions
+
+  echo Done Onboading $d
+done;
