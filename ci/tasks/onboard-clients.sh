4c06b76aeede88ef71ea73b0409d3ed163c49aa6
azwickey-pivotal <vania12R>
2016-11-14 09:19:53 -0500

    initial checkin of skeleton project

--- ci/tasks/onboard-clients.sh
+++ ci/tasks/onboard-clients.sh
@@ -2,3 +2,11 @@
 ls -lha
 wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/2804/product_files/8892/download
 sudo dpkg -i pivotal-gemfire.deb
+
+# Loop through Client teams
+
+# Add Security
+
+# Add Jars
+
+# Configure Regions
