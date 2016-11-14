626916fc5e6bf8cc4be7dd3c625c9c1b35f8c94f
azwickey-pivotal <vania12R>
2016-11-14 10:01:22 -0500

    building onboard script

--- ci/tasks/onboard-clients.sh
+++ ci/tasks/onboard-clients.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-#wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/2804/product_files/8892/download
-#sudo dpkg -i pivotal-gemfire.deb
+wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/2804/product_files/8892/download
+sudo dpkg -i pivotal-gemfire.deb
 
 ls -lha
 
